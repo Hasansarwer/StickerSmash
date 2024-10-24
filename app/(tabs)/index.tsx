7c3202b0b89d32061e80b9f1e1043315d1b28609
Hasansarwer <hasanmathju@gmail.com>
2024-10-24 20:28:08 +0600

    Button added need to complete

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import { View, StyleSheet } from "react-native";
 
-import ImageViewer from "@/components/ImageViewer";
+/* @tutinfo */import Button from '@/components/Button'; 
+import ImageViewer from '@/components/ImageViewer';
 
-const PlaceHolderImage = require("@/assets/images/background-image.png");
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource= {PlaceHolderImage} />
-        
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -20,15 +22,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  /* @tutinfo Add the styles the following styles. */
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,31 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 type Props = {
   label: string;
+  theme: 'primary';
 };
 
-export default function Button({ label }: Props) {
-  return (
-    <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert('You Pressed a button.')}>
-            <Text style={styles.buttonLabel}>{label}</Text>
-        </Pressable>
-    </View>
-  );
+export default function Button({ label, theme }: Props) {
+    if(theme === 'primary'){
+        return (
+            <View style={[styles.button, {backgroundColor: '#fff'}]}>
+                <Pressable style={styles.button} onPress={() => alert('You Pressed a button.')}>
+                <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+                <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => alert('You Pressed a button.')}>
+                <FontAwesome name="plus" size={24} color="white" style={styles.buttonIcon} />
+                <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
