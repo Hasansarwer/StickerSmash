f04e624caabde263647c86218101ed8a7263e180
Hasansarwer <hasanmathju@gmail.com>
2024-10-24 20:05:29 +0600

    image added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceHolderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}> Home screen.</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource= {PlaceHolderImage} />
+        
+      </View>
     </View>
   );
 }
@@ -18,15 +21,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
-  }
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
